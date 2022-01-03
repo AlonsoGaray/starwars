338c1847544e75bc2f603caf2542bdd0b4710a8e
Alonso Garay <alonsogarayp@gmail.com>
2022-01-03 12:22:17 -0500

    Pagina terminada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import logo from './img/star-wars-logo.png'
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 1200px;
-  border: 3px solid black;
+  align-items: center;
+  margin: auto;
+  min-width: 300px;
 `
+
 const AllCards = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -17,7 +20,7 @@ const Card = styled.div`
   display:flex;
   flex-direction: column;
   justify-content: center;
-  border: 3px solid green;
+  border: 3px solid white;
   width: 300px;
   height:auto;
   margin: 10px 20px;
@@ -26,48 +29,67 @@ const Card = styled.div`
 const Nombre = styled.h2`
   text-align: center;
   margin: 5px 0;
+  color: 	#FFE81F;
 `
 
-const Info = styled.p`
-  font-size: 16px;
-  margin: 10px 10px;
+const Info = styled.h3`
+  font-size: 18px;
+  margin: 20px 10px 0 10px;
+  color: 	#FFE81F;
 `
+
+const P = styled.p`
+  font-size: 18px;
+  margin: 0 10px;
+  color: 	white;
+`
+
 const Buttons = styled.div`
-  border: 3px solid yellow;
-  display:flex;
+  display: flex;
   justify-content: center;
 `
 
 const Button = styled.button`
+  background-color: 	#FFE81F;
+  font-size: 20px;
   width:100px;
-  height:100px;
+  height:80px;
   max-width:100px;
   max-height:100px;
+  margin: 10px 20px;
+`
+
+const Logo = styled.img`
+  max-width: 200px;
+  max-height: 200px;
 `
 
 const Personajes = () => {
+  const [response, setResponse] = useState([])
   const [chars, setChars] = useState([])
   const [page, setPage] = useState(1)
   const [loading, setLoading] = useState(false);
+  
   const handlePrevious = () => {
-    setPage(page-1)
+    response.previous === null ? setPage(page) : setPage(page-1)
   }
 
   const handleNext = () => {
-    setPage(page+1)
+    response.next === null ? setPage(page) : setPage(page+1)
   }
 
   useEffect(() =>  {
     setLoading(true);
     fetch(`https://swapi.dev/api/people/?page=${page}`)
       .then((res) => res.json())
-      .then((data) => setChars(data.results))
+      .then((data) => {setChars(data.results); setResponse(data)})
       .catch((error)=> console.log(error))
       .finally(() => setLoading(false));
   }, [page])
 
+  console.log(response)
   if (loading) {
-    return <>
+    return <div className="Loading">
     <h1>Loading</h1>
     <div className="lds-roller">
       <div />
@@ -79,51 +101,45 @@ const Personajes = () => {
       <div />
       <div />
     </div>
-  </>;
+  </div>;
   }
 
   return (
     <Container>
+      <Logo src={logo} alt="logo" />
+      <Nombre> PERSONAJES </Nombre>
       <AllCards>
       {chars.map((personaje, i) => {
         return (
           <Card key={i}>
-            <Nombre>{personaje.name}</Nombre>
-            <Info>
-              <strong>Gender</strong> <br/>
-              {personaje.gender} 
-            </Info>
-            <Info>
-              <strong>Birth Year</strong> <br/>
-              {personaje.birth_year} 
-            </Info>
-            <Info>
-              <strong>Height</strong> <br/>
-              {personaje.height}
-            </Info>
-            <Info>
-              <strong>Weight</strong> <br/>
-              {personaje.mass}
-            </Info>            
+            <Nombre> {personaje.name} </Nombre>
+            <Info> Gender </Info>
+            <P>{personaje.gender} </P>
+            <Info> Birth Year </Info>
+            <P> {personaje.birth_year} </P>
+            <Info> Height cm </Info>
+            <P> {personaje.height} </P>
+            <Info> Weight kg </Info>   
+            <P> {personaje.mass} </P>
           </Card>
         )
       })}
       </AllCards>
       <Buttons>
-      <Button
-        className="Pokemons__button"
-        type="button"
-        onClick={handlePrevious}
-      >
-        Anterior Pagina
-      </Button>
-      <Button
-        className="Pokemons__button"
-        type="button"
-        onClick={handleNext}
-      >
-        Siguiente Pagina
-      </Button>
+        <Button
+          className="Pokemons__button"
+          type="button"
+          onClick={handlePrevious}
+        >
+          Anterior
+        </Button>
+        <Button
+          className="Pokemons__button"
+          type="button"
+          onClick={handleNext}
+        >
+          Siguiente
+        </Button>
       </Buttons>
     </Container>
   );
